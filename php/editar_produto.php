abae2acae7b135288e1566e49f8364000b879260
Douglas \Maziero <promax.contato@hotmail.com>
2024-10-17 11:39:09 -0300

    Atualizar o edit

--- php/editar_produto.php
+++ php/editar_produto.php
@@ -5,7 +5,7 @@ $id = $_GET['id'];
 $sql = "DELETE FROM produtos WHERE id = $id";
 
 if (mysqli_query($conn, $sql)) {
-    header("Location: ../index.php"); // Redireciona de volta à página principal
+    header("Location: ../index.php"); 
 } else {
     echo "Erro ao excluir: " . mysqli_error($conn);
 }
